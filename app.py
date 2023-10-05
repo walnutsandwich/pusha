ec12bc90d2115f4187053436e6bd122031a1c147
Kokia <312584796@qq.com>
2023-10-05 20:00:17 +0800

    修改2

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-
 from flask import Flask
 import os
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
 def index():
-    print("GET /")
-    return "Welcome, this is my Flask app deployed on Zeabur"
-
+    return "Welcome, this is a Flask app deployed on Zeabur"
+ 
 if __name__ == '__main__':
-    app.run(debug=True, port=6000, host='0.0.0.0')
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
 
 # from flask import Flask, render_template, request, jsonify
 # from post_token import FakeGPT
